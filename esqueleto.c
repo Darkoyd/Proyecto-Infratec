b964e05ff9885cdcc7e29796ce11f90d25a14896
mg.torres <mg.torres@uniandes.edu.co>
2019-11-01 15:35:25 -0500

    Decimal a binario

--- esqueleto.c
+++ esqueleto.c
@@ -58,6 +58,45 @@ void calcularCRC(Informacion *datos, int lonDiv, unsigned char divisor)
             printf("0");
     }
 
+#include <stdio.h>  
+#include <string.h>  
+
+int main()  
+{  
+    long decimal, tempDecimal;  
+    char binary[65];  
+    int index = 0;  
+
+    /* 
+     * Reads decimal number from user 
+     */  
+    printf("Enter any decimal value : ");  
+    scanf("%ld", &decimal);  
+
+    /* Copies decimal value to temp variable */  
+    tempDecimal = decimal;  
+
+    while(tempDecimal!=0)  
+    {  
+        /* Finds decimal%2 and adds to the binary value */  
+        binary[index] = (tempDecimal % 2) + '0';  
+
+        tempDecimal /= 2;  
+        index++;  
+    }  
+    binary[index] = '\0';  
+
+    /* Reverse the binary value found */  
+    strrev(binary);  
+
+    printf("\nDecimal value = %ld\n", decimal);  
+    printf("Binary value of decimal = %s", binary);  
+
+    return 0;  
+}
+
+
+
     unsigned char bits[8] = {'0', '0', '0', '0', '0', '0', '0', '0'};
 
     strcat(datos->contenido, bits);
